b34dc48252e5cd1617c14a46b695b85879855f38
Jaanu2004 <125555491+Jaanu2004@users.noreply.github.com>
2023-03-14 07:49:40 +0530

    Add files via upload

---
+++ max.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+main()
+{
+int n,i,min,max;//n=6
+scanf("%d",&n);  
+int a[n];//declaring an array with specific size
+for(i=0;i<n;i++)//inserting array values into an array
+scanf("%d",&a[i]);
+min=a[0];
+for(i=1;i<n;i++)//displaying array values from the array
+{
+if(a[i]<min)
+min=a[i];
+if(a[i]>max)
+max=a[i];
+}
+printf("min-%d max-%d",min,max);
+}
