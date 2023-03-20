7f7f9ba51419e3ee28524bebdd4026fe796e863e
Jaanu2004 <125555491+Jaanu2004@users.noreply.github.com>
2023-03-20 21:48:20 +0530

    Add files via upload

---
+++ sumofeven.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+
+
+// function to add two integer numbers
+int add(int n1, int n2)
+{
+  int result;
+  result = n1 + n2;
+  return result;
+}
+
+// function to subtract two integer numbers
+int subtract(int n1, int n2)
+{
+  int result;
+  result = n1 - n2;
+  return result;
+}
+
+// function to multiply two integer numbers
+int multiply(int n1, int n2)
+{
+  int result;
+  result = n1 * n2;
+  return result;
+}
+
+// function to divide two integer numbers
+int divide(int n1, int n2)
+{
+  int result;
+  result = n1 / n2;
+  return result;
+}
+// main function
+int main()
+{
+  int num1, num2;
+
+  printf("Enter two numbers: ");
+  scanf("%d %d", &num1, &num2);
+
+  printf("%d + %d = %d\n", num1, num2, add(num1, num2));
+  printf("%d - %d = %d\n", num1, num2, subtract(num1, num2));
+  printf("%d * %d = %d\n", num1, num2, multiply(num1, num2));
+  printf("%d / %d = %d\n", num1, num2, divide(num1, num2));
+
+  return 0;
+}
+
+
